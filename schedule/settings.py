6435892d483fbec64064ff74435c2b454c0653e4
scott9611 <scottsteven96@outlook.com>
2024-10-16 10:48:47 -0500

    SECRET_KEY environment variable

--- schedule/settings.py
+++ schedule/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'NMBgl5CPya2IVLuSWxDh90_I1fiArSEKULu9R9uJj26fsnenUyxsNdAOdHKEIresicE'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
