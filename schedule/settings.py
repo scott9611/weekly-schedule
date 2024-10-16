0f1ae6c28db46c5ae54d15c65a554c9847d204fc
scott9611 <scottsteven96@outlook.com>
2024-10-16 10:55:16 -0500

    load_dotenv commented

--- schedule/settings.py
+++ schedule/settings.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
