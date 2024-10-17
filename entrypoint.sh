6a8e81392c4120c27c37549bee19b940014a27c4
scott9611 <scottsteven96@outlook.com>
2024-10-16 21:36:31 -0700

    create super user

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,10 @@ echo "Applying database migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
+# Create superuser
+echo "Creating superuser"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '12345') if not User.objects.filter(username='admin').exists() else print('Superuser already exists.')"
+
 # Ensure Nginx can access static files
 chmod -R 755 /app/staticfiles
 
